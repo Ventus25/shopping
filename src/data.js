9dd8234103df744a30366ee0d508fe9484a89d63
txt25 <72610092+txt25@users.noreply.github.com>
2020-10-28 11:36:52 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,84 +1,85 @@
 export const storeProducts = [
   {
     id: 1,
-    title: "Google Pixel - Black",
+    title: "Trở thành nhà thiết kế game toàn tập - Từ coding tới thiết kế 2020",
     img: "img/product-1.png",
     price: 10,
-    company: "GOOGLE",
+    company: "Udemy",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Nếu bạn nghiêm túc về việc trở thành một nhà thiết kế trò chơi, thì khóa học này là dành cho bạn. Để thành công trong ngành công nghiệp cạnh tranh này, điều cần thiết là phải có kiến thức chuyên môn về phát triển trò chơi. /n Trong suốt khóa học này bạn sẽ được học về Unity, 3D game design, 2D game design, coding, C#, game development, 3D animation, programming, Unity3D. Gần như toàn bộ kiến thức cần thiết để trở thành một nhà thiết kế game chân chính.",
+    "Giao diện thân thiện và trực quan của Unity là một nơi tuyệt vời để bắt đầu con đường trở thành nhà thiết kế trò chơi. Sau đó, bạn sẽ tìm hiểu cách chuyển các mô hình và hoạt hình 3D của mình vào Unity để lắp ráp trò chơi. /n Cuối cùng, bạn sẽ chuyển sang phần vẽ kỹ thuật số của loạt phim tổng thể này, nơi bạn học cách vẽ kỹ thuật số thế giới tưởng tượng, sinh vật và nhân vật. Và giống như tất cả các phần trước của chúng tôi trong loạt bài tổng thể này, bạn sẽ bắt đầu bằng cách học những điều cơ bản với các bài học dựa trên dự án thú vị, trước khi chuyển sang vẽ kỹ thuật số trung cấp và nâng cao hơn.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 2,
-    title: "Samsung S7",
+    title: "Khóa học lập trình Android Kotlin toàn tập 2020",
     img: "img/product-2.png",
     price: 16,
-    company: "SAMSUNG",
+    company: "Udemy",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
+      "Khóa học lập trình Android Kotlin này là khóa học dành cho bất cứ ai muốn trở thành một lập trình viên Kotlin từ đầu. Khóa học sẽ bắt đầu bằng cách loại bỏ tất cả các nguyên tắc cơ bản về lập trình mà bạn cần để bắt đầu lập trình Kotlin. /n Bạn sẽ được học từ cách cài đặt môi trường phát triển, tới cách bạn sẽ chạy ứng dụng Kotlin đầu tiên của mình và hiểu cách thức chương trình hoạt động trong Kotlin. Sau đó, bạn sẽ được dạy về các biến và hoạt động toán học và các ưu tiên. Tiếp theo, khóa học sẽ đưa ra logic và đưa ra quyết định, nói về các vòng lặp. /n Thông qua khóa học này bạn sẽ được tìm hiểu cách xây dựng 17 trò chơi trực tuyến và ứng dụng cho Android Q, như Pokémon, twitter, Tic Tac Toe và notepad bằng cách sử dụng Kotlin.",
+
     count: 0,
     total: 0
   },
   {
     id: 3,
-    title: "HTC 10 - Black",
+    title: "Khóa học Big Data và Hadoop cho người mới bắt đầu với thực hành 2020",
     img: "img/product-3.png",
     price: 8,
-    company: "htc",
+    company: "Udemy",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Khóa học bigdata này cung cấp cho bạn mọi thứ bạn cần biết về Dữ liệu lớn và Tìm hiểu Hadoop, HDFS, MapReduce, Hive & Pig bằng cách thiết kế Đường ống dữ liệu./n Mục tiêu chính của khóa học này là giúp bạn hiểu Kiến trúc phức tạp của Hadoop và các thành phần của nó, hướng dẫn bạn đi đúng hướng để bắt đầu và nhanh chóng bắt đầu làm việc với Hadoop và các thành phần của nó. /n Khóa học bao gồm mọi thứ bạn cần khi là Người mới bắt đầu Dữ liệu Lớn. Tìm hiểu về thị trường Dữ liệu lớn, vai trò công việc khác nhau, xu hướng công nghệ, lịch sử của Hadoop, HDFS, Hệ sinh thái Hadoop,….",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 4,
-    title: "HTC 10 - White",
+    title: "Học Java – Kinh nghiệm Design Patterns trong Java",
     img: "img/product-4.png",
     price: 18,
-    company: "htc",
+    company: "Udemy",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Khóa học này là khóa học giúp bạn học cách viết phần mềm tốt hơn bằng cách hiểu các vấn đề phổ biến và áp dụng các mẫu thiết kế để có giải pháp tốt hơn. Bạn sẽ có thể thực tế hiểu  cách thức hoạt động của mẫu với các bài học Java được bao gồm.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 5,
-    title: "HTC Desire 626s",
+    title: "Khóa học JavaScript hoàn chỉnh 2020: Xây dựng các dự án thực tế",
     img: "img/product-5.png",
     price: 24,
-    company: "htc",
+    company: "Udemy",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Đây là một khóa học JavaScript thực sự hoàn chỉnh, mà bạn cần để làm chủ JavaScript dù bạn là người mới bắt đầu hay là người đã có kinh nghiệm. /n Khóa học sẽ đưa bạn từ một người mới bắt đầu hoàn thành JavaScript đến một nhà phát triển nâng cao. Bạn sẽ không chỉ học ngôn ngữ JavaScript, bạn cũng sẽ học cách lập trình. Cách giải quyết vấn đề. Cách cấu trúc và tổ chức mã bằng các mẫu JavaScript phổ biến.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 6,
-    title: "Vintage Iphone",
+    title: "Khóa học Adobe InDesign CC toàn tập 2020",
     img: "img/product-6.png",
     price: 17,
-    company: "apple",
+    company: "Udemy",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Nếu bạn đang muốn tìm hiểu Adobe InDesign từ đầu thì bài viết này là dành cho bạn. Đây là một khóa học hoàn hảo cho các nhà thiết kế, doanh nghiệp & doanh nhân, nhiếp ảnh gia, tác giả và doanh nghiệp trực tuyến. Khóa học này sẽ đưa bạn từ người mới bắt đầu đến người dùng nâng cao chỉ trong vài giờ.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 7,
-    title: "Iphone 7",
+    title: "Khóa học AZ 300 – Luyện thi công nghệ kiến trúc Azure",
     img: "img/product-7.png",
     price: 30,
-    company: "apple",
+    company: "Udemy",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Microsoft Azure là một kỹ năng có nhu cầu cao trong thị trường kinh doanh và công nghệ ngày nay. Số lượng sử dụng Azure đã tăng 90% so với năm trước. Các trang khảo sát lương báo cáo rằng Kiến trúc sư Azure kiếm được 130.000 đến 170.000 đô la Mỹ mỗi năm trên khắp Hoa Kỳ. Microsoft có hơn 100 trung tâm dữ liệu, trên 50 khu vực và 10 geos, khiến nó trở thành một trong những trung tâm dữ liệu lớn nhất trên thế giới.. Khóa học này là khóa học chuẩn bị học tập hoàn chỉnh cho Kỳ thi AZ-300: Microsoft Azure Architect Technologies . Vượt qua bài kiểm tra này mang đến cho bạn Chuyên gia Microsoft Microsoft: Kiến trúc chứng nhận Microsoft Azure Solutions .",
     inCart: false,
     count: 0,
     total: 0
